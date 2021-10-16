fb09fcb25145029c157cb02a0f92d798399be7de
RobsonAndradev <robsom.s.a@gmail.com>
2021-10-16 12:26:44 -0300

    Add square rote

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -1,7 +1,14 @@
 const app = require('express')()
+const mathService = require('./math-service')
 
 app.get('/', (req, res) => {
   return res.send({ ping: 'pong'})
+})
+
+app.get('/square/:number', (req, res) => {
+  const { number } = req.params
+
+  return res.send({ result: mathService.square(number) })
 })
 
 app.listen(3000, () => {
